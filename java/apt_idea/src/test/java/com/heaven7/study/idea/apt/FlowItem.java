928b53d5d7970704c87166ab3a4ffa63d25282db
LinghtSun <532278976@qq.com>
2017-10-14 17:36:41 +0800

    annotation processor ok

---
+++ java/apt_idea/src/test/java/com/heaven7/study/idea/apt/FlowItem.java
@@ -0,0 +1,20 @@
+package com.heaven7.study.idea.apt;
+
+import com.heaven7.java.data.mediator.Field;
+import com.heaven7.java.data.mediator.Fields;
+
+import static com.heaven7.java.data.mediator.FieldFlags.FLAGS_MAIN_SCOPES_2;
+import static com.heaven7.java.data.mediator.FieldFlags.FLAGS_NO_EXPOSE;
+
+/**
+ * the item of flow
+ * Created by heaven7 on 2017/10/8.
+ */
+
+@Fields({
+        @Field(propName = "id", type = int.class,  since = 1.2, until = 2.0),
+        @Field(propName = "name" ,seriaName = "stu_name"),
+        @Field(propName = "desc" , flags = FLAGS_MAIN_SCOPES_2 | FLAGS_NO_EXPOSE),
+})
+public interface FlowItem {
+}
